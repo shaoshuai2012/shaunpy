3dc2a616aea485ecb2bc34407347cfe3b9db82fb
ShaoShuai <nuaashao@163.com>
2017-12-31 19:44:05 +0800

    邵帅的个人施工文件夹

--- 微信台账登记.py
+++ 微信台账登记.py
@@ -1,11 +1,20 @@
 import itchat
+from pyexcel_xls import save_data
+from pyexcel_xls import get_data
 
-# 注册消息响应事件，消息类型为itchat.content.TEXT，即文本消息
+data = get_data(r"/Users/ShaoShuai/Desktop/test.xlsx")
+data['工作表1'].append(['666'])
+print(len(data['工作表1']))
+
+
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-    # 返回同样的文本消息
-    return msg['Text']
+    if msg['Text'][0:3] == '流水账':
+        print(msg['Text'][4:], (itchat.search_friends(userName=msg['FromUserName']))["NickName"])
+        data['工作表1'].append([msg['Text'][4:], (itchat.search_friends(userName=msg['FromUserName']))["NickName"]])
+        save_data("微信流水账登记.xls", data)
+        return msg['Text']
 
-itchat.auto_login()
+itchat.auto_login(hotReload=True)
 # 绑定消息响应事件后，让itchat运行起来，监听消息
 itchat.run()
