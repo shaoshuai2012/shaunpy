bd3a4042d59a16cb349f6556cd4c0939434d6599
ShaoShuai <nuaashao@163.com>
2017-12-31 20:55:57 +0800

    邵帅的个人施工文件夹

--- 微信台账登记.py
+++ 微信台账登记.py
@@ -1,20 +1,21 @@
 import itchat
+import time
 from pyexcel_xls import save_data
 from pyexcel_xls import get_data
 
-data = get_data(r"/Users/ShaoShuai/Desktop/test.xlsx")
-data['工作表1'].append(['666'])
+data = get_data(r"/Users/ShaoShuai/Desktop/shaunpy/微信流水账登记.xls")
 print(len(data['工作表1']))
 
 
-@itchat.msg_register(itchat.content.TEXT)
+@itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
 def text_reply(msg):
     if msg['Text'][0:3] == '流水账':
-        print(msg['Text'][4:], (itchat.search_friends(userName=msg['FromUserName']))["NickName"])
-        data['工作表1'].append([msg['Text'][4:], (itchat.search_friends(userName=msg['FromUserName']))["NickName"]])
+        print(msg['ActualNickName'], msg['Text'][4:])
+        data['工作表1'].append(
+            [msg['ActualNickName'], msg['Text'][4:], time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())])
         save_data("微信流水账登记.xls", data)
-        return msg['Text']
+        return '流水账记录成功！', msg['Text']
+
 
 itchat.auto_login(hotReload=True)
-# 绑定消息响应事件后，让itchat运行起来，监听消息
 itchat.run()
