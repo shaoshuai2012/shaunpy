21f7d22c4ab0078824e3222d2a9e0b0d205d0a04
shaoshuai2012 <nuaashao@163.com>
2018-01-01 19:02:43 +0800

    微信台账登记，增加多台账同时记录功能

--- 微信台账登记.py
+++ 微信台账登记.py
@@ -3,7 +3,8 @@ import time
 from pyexcel_xls import save_data
 from pyexcel_xls import get_data
 
-data = get_data(r"/Users/ShaoShuai/Desktop/shaunpy/微信流水账登记.xls")
+# data = get_data(r"/Users/ShaoShuai/Desktop/shaunpy/微信台账登记.xls")
+data = get_data(r"c:/shaunpy/微信台账登记.xls")
 print(len(data['工作表1']))
 
 
@@ -13,8 +14,14 @@ def text_reply(msg):
         print(msg['ActualNickName'], msg['Text'][4:])
         data['工作表1'].append(
             [msg['ActualNickName'], msg['Text'][4:], time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())])
-        save_data("微信流水账登记.xls", data)
+        save_data("微信台账登记.xls", data)
         return '流水账记录成功！', msg['Text']
+    elif msg['Text'][0:3] == '取备件':
+        print(msg['ActualNickName'], msg['Text'][4:])
+        data['工作表2'].append(
+            [msg['ActualNickName'], msg['Text'][4:], time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())])
+        save_data("微信台账登记.xls", data)
+        return '备件台账记录成功！', msg['Text']
 
 
 itchat.auto_login(hotReload=True)
--- 沙盘.py
+++
@@ -1,9 +0,0 @@
-import pyexcel_xls
-from pyexcel_xls import save_data
-from pyexcel_xls import get_data
-
-data = get_data(r"/Users/ShaoShuai/Desktop/test.xlsx")
-print(len(data['工作表1']))
-data['工作表1'].append(['666'])
-print(len(data['工作表1']))
-save_data("your_file.xls", data)
