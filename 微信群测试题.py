704158502c479d4ac1479bdc18c20b34037d8555
ShaoShuai <nuaashao@163.com>
2018-01-07 15:26:06 +0800

    邵帅的个人施工文件夹

--- 微信群测试题.py
+++ 微信群测试题.py
@@ -2,17 +2,25 @@ import itchat
 from pyexcel_xls import get_data
 from pyexcel_xls import save_data
 
-print('库存试题数：', len(get_data(r"C:/shaunpy/试题.xls")['试题']) - 1)
-print('当前题目编号：', get_data(r"C:/shaunpy/试题.xls")['试题'][0][2])
-
+# print('库存试题数：', len(get_data(r"C:/shaunpy/试题.xls")['试题']) - 1)
+# print('当前题目编号：', get_data(r"C:/shaunpy/试题.xls")['试题'][0][2])
+print('库存试题数：', len(get_data(r"/Users/ShaoShuai/Desktop/shaunpy/试题.xls")['试题']) - 1)
+print('当前题目编号：', get_data(r"/Users/ShaoShuai/Desktop/shaunpy/试题.xls")['试题'][0][2])
+score = {}
 
 @itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
 def text_reply(msg):
-    data = get_data(r"C:/shaunpy/试题.xls")
+    # data = get_data(r"C:/shaunpy/试题.xls")
+    data = get_data(r"/Users/ShaoShuai/Desktop/shaunpy/试题.xls")
     if msg['Text'] == '开始答题':
         return data['试题'][data['试题'][0][2]][0]
     elif msg['Text'] == str(data['试题'][data['试题'][0][2]][1]) or msg['Text'] == str.lower(
             data['试题'][data['试题'][0][2]][1]):
+        name = msg['ActualNickName']
+        if name in score.keys():
+            score[name] = score[name] + 1
+        else:
+            score[name] = 1
         if data['试题'][0][2] < len(data['试题']) - 1:
             data['试题'][0][2] = data['试题'][0][2] + 1
             save_data("试题.xls", data)
@@ -26,6 +34,11 @@ def text_reply(msg):
             data['试题'][data['试题'][0][2]][1]) and msg['Text'] != str.lower(data['试题'][data['试题'][0][2]][1]):
         save_data("试题.xls", data)
         return '答案不是' + msg['Text'] + '哦，再想想~'
+    elif msg['Text'] == '查看成绩':
+        chengji = ''
+        for key in score:
+            chengji = chengji + '姓名：' + key + '  成绩：' + str(score[key]) + '分\n'
+        return chengji
 
 
 itchat.auto_login(hotReload=True)
